f1f931ba4d724fe0303c165b6cb72f3032c2a74c
George Paley <George@JDPaley.com>
2017-06-26 14:29:40 -0400

    wip

---
+++ src/main/java/com/jdpaley/WebDataObject.java
@@ -0,0 +1,15 @@
+package com.jdpaley;
+
+public class WebDataObject {
+	
+	private String choice;
+
+	public String getChoice() {
+		return choice;
+	}
+
+	public void setChoice(String choice) {
+		this.choice = choice;
+	}
+
+}
